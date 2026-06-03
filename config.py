c17a6aca4c74c31c6279afc35c9364c36d1dc076
alexandre-henriksen <alexandre.henriksen@gmail.com>
2026-06-03 00:22:20 -0300

    Incluir .txt ao final do caminho dos arquivos

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 
 # Caminhos específicos
-CAMINHO_MOCHILA01 = DATA_DIR / "mochila01.txt"
-CAMINHO_MOCHILA02 = DATA_DIR / "mochila02.txt"
-CAMINHO_MOCHILA1000 = DATA_DIR / "mochila1000"
-CAMINHO_MOCHILA2500 = DATA_DIR / "mochila2500"
-CAMINHO_MOCHILA5000 = DATA_DIR / "mochila5000"
+CAMINHO_MOCHILA01 = DATA_DIR / "mochila01.txt.txt"
+CAMINHO_MOCHILA02 = DATA_DIR / "mochila02.txt.txt"
+CAMINHO_MOCHILA1000 = DATA_DIR / "mochila1000.txt"
+CAMINHO_MOCHILA2500 = DATA_DIR / "mochila2500.txt"
+CAMINHO_MOCHILA5000 = DATA_DIR / "mochila5000.txt"
 
 # Demais pastas
 OUTPUT_DIR = BASE_DIR / "output"
